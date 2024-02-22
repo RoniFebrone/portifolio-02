c709d8b8668c970cfb51e3b634f1f5c2bda2412d
Roni Febrone de Almeida <132415797+RoniFebrone@users.noreply.github.com>
2024-02-22 20:36:35 -0300

    fix: Corrigido o bug do preview do site em projetos, colocado imagem e titulo no bar da guia, removido link do instagran.

--- src/Pages/Navbar/Navbar.tsx
+++ src/Pages/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import './index.scss'
-import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 
 import {Link} from 'react-scroll'
@@ -13,7 +13,6 @@ function Navbar() {
                 <li><Link to="Projects" smooth={true} duration={500} offset={-80}>Projetos</Link></li>
             </ul>
             <ul className="list2">
-                <li> <a href='https://instagram.com/roninho_f?igshid=OGQ5ZDc2ODk2ZA==' target='Blank'> <FaInstagram size={24}/> </a></li>
                 <li> <a href='https://www.linkedin.com/notifications/?filter=all' target='Blank'> <FaLinkedin size={24}/> </a></li>
                 <li> <a href='https://github.com/RoniFebrone' target='Blank'> <FaGithub size={24}/> </a></li>
             </ul>
